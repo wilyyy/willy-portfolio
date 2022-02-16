90558e6889e117318f2834ba61f5c0c21df08208
wilyyy <alvarez_william@outlook.com>
2022-02-15 23:11:52 -0800

    glass ui blows

--- utils/themeconfig.js
+++ utils/themeconfig.js
@@ -2,11 +2,15 @@ export const global_theme = {
     dark: {
         base: '#0D0F11',
         // base: '#0A0B0D',
-        text: '#fff' //temporary
+        text: '#fff', //temporary
+        modalFillValue1: "#151A1F",
+        modalStroke: ""
     },
     light: {
         base: '#FAF4D3',
-        text: '#000' //temporary
+        text: '#000', //temporary
+        modalFill: "linear-gradient(152.97deg, rgba(136, 136, 136, 0.06) 0%, rgba(255, 255, 255, 0) 100%)",
+        modalStroke: "#000"
     }
 };
 
