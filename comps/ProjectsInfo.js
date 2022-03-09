d8113b8eca46b55e12f4e4fac93826a460bcda2c
wilyyy <-g>
2022-03-08 18:35:51 -0800

    projects

--- comps/ProjectsInfo.js
+++ comps/ProjectsInfo.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useEffect } from "react";
 import { motion } from "framer-motion";
 import { global_theme, compassplus_colors } from "../utils/themeconfig";
 
@@ -9,14 +10,14 @@ const Container = styled(motion.div)`
     background-color: ${props=>props.bgcolor};
     display: flex;
 
-    -webkit-filter: grayscale(100%);
+    /* -webkit-filter: grayscale(100%);
     filter: grayscale(100%);
 
     :hover{
         -webkit-filter: grayscale(0%); 
         filter: grayscale(0%);
         transition: 1s;
-    }
+    } */
 `;
 
 const ProjectsInfo = ({
@@ -24,8 +25,9 @@ const ProjectsInfo = ({
     bgcolor,
     width,
     height,
-    onButtonClick
+    onButtonClick=()=>{}
 }) => {
+    
     return (
         <Container 
             onClick={onButtonClick} 
